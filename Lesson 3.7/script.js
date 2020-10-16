17d8221ef66aa8462a1f72d888588d407e5c7f65
And-NN <andrew.nnma@gmail.com>
2020-10-16 15:25:26 +0300

    Lessons 3.6-3.7

---
+++ Lesson 3.7/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+function showThis(a, b) {
+	console.log(this);
+	function sum() {
+		console.log(this);
+		return a + b; // замыкание
+	}
+	console.log(sum());
+}
+showThis(4, 5);
+showThis(5, 5);
+
+let obj = {
+	a: 20,
+	b: 15,
+	sum: function() {
+		console.log(this);
+	}
+}
+obj.sum();
+
+
+
+let user = {
+	name: "Jhon"
+};
+
+function sayName(surname) {
+	console.log(this);
+	console.log(this.name + " " + surname);
+}
+
+console.log(sayName.call(user, 'Do'));
+console.log(sayName.apply(user, ['Smith', 'Snow'])); // метод позволяет передать МАССИВ параметров
+
+function count(number) {
+	return this * number;
+}
+
+let double = count.bind(2) // двойка заменит this в контексте вызова
+console.log(double(3)); // передаётся number
+console.log(double(10));
+
+
+let btn = document.querySelector('button');
+btn.addEventListener('click', function() {
+	console.log(this);  // контекст вызова - сам элемент
+	this.style.backgroundColor = 'red'
+});
