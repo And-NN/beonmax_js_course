cf6c38b6a177815de8c5b88c028d3c50f42f3991
And-NN <andrew.nnma@gmail.com>
2020-10-27 17:15:20 +0300

    Lesson 4.16 ES6 modules

---
+++ Lesson 4.16 ES6 modules/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log(1),console.log(2),console.log(void console.log("Hello!"))})();
---
+++ Lesson 4.16 ES6 modules/src/index.js
@@ -0,0 +1,5 @@
+import {one, two, sayHi} from './script';
+
+console.log(one);
+console.log(two);
+console.log(sayHi());
